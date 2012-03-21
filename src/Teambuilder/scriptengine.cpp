526a0b6029d9454bd82ee680da3803b0a3393b69
Blastcore <blastcore0@hotmail.com>
2012-03-21 20:52:14 -0300

    Added more stuff to client-side scripting

--- src/Teambuilder/scriptengine.cpp
+++ src/Teambuilder/scriptengine.cpp
@@ -1,7 +1,15 @@
 #include "scriptengine.h"
 
-ScriptEngine::ScriptEngine()
+ScriptEngine::ScriptEngine(Client *client)
 {
+    setParent(client);
+    myClient = client;
+
+    QScriptValue sys = myEngine.newQObject(this);
+    myEngine.globalObject().setProperty("sys", sys);
+
+    loadScripts();
+
 
 }
 
@@ -10,3 +18,22 @@ ScriptEngine::~ScriptEngine()
 
 }
 
+void ScriptEngine::loadScripts()
+{
+    QFile file("client-scripts.js");
+    file.open(QIODevice::ReadOnly);
+
+    updateScripts(QString::fromUtf8(file.readAll()));
+}
+
+void ScriptEngine::updateScripts(const QString &scripts)
+{
+    myScript = myEngine.evaluate(scripts);
+    myEngine.globalObject().setProperty("script", myScript);
+
+    if(myScript.isError()) {
+        qDebug() << "Fatal Script Error Line " + QString::number(myEngine.uncaughtExceptionLineNumber()) + ": " + myScript.toString();
+    } else {
+        qDebug() << "Script Check: OK";
+    }
+}
--- src/Teambuilder/scriptengine.h
+++ src/Teambuilder/scriptengine.h
@@ -4,12 +4,25 @@
 #include <QtCore>
 #include <QtScript>
 
+#include "client.h"
+
+class Client;
+
 class ScriptEngine : public QObject
 {
     Q_OBJECT
+    friend class Client;
+
 public:
-    ScriptEngine();
+    ScriptEngine(Client *client);
     ~ScriptEngine();
+    void loadScripts();
+    void updateScripts(const QString &scripts);
+
+private:
+    Client *myClient;
+    QScriptEngine myEngine;
+    QScriptValue myScript;
 };
 
 #endif // SCRIPTENGINE_H
